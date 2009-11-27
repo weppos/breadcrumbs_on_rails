010ff0c30473b4f78ce57c126eb328e723fa5d12
Simone Carletti <weppos@weppos.net>
2009-11-27 20:36:11 +0100

    Release BreadcrumbsOnRails 0.2.0 (first open source release)

--- lib/breadcrumbs_on_rails/version.rb
+++ lib/breadcrumbs_on_rails/version.rb
@@ -18,8 +18,8 @@ module BreadcrumbsOnRails
 
   module Version
     MAJOR = 0
-    MINOR = 1
-    PATCH = 1
+    MINOR = 2
+    PATCH = 0
     ALPHA = nil
 
     STRING = [MAJOR, MINOR, PATCH, ALPHA].compact.join('.')
