76d939735cc8db4547b4a2669023d7e6ac91a4ca
Simone Carletti <weppos@weppos.net>
2009-11-27 19:09:48 +0100

    test_helper.rb cleanup

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,12 +8,9 @@ gem     'mocha', '~>0.9.7'
 # gem     'rails', '2.2.2'
 # gem     'mocha', '0.9.5'
 
-require 'mocha'
 require 'test/unit'
 require 'active_support'
 require 'action_controller'
-require 'action_controller/cgi_ext'
-require 'action_controller/test_process'
 require 'action_view/test_case'
 
 $:.unshift File.dirname(__FILE__) + '/../lib'
