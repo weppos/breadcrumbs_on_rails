c3679f403a744d0f3d06d14b3fc4f8d5747c118d
Simone Carletti <weppos@weppos.net>
2010-05-09 23:49:26 +0200

    Rails 3 workaround

--- lib/breadcrumbs_on_rails/version.rb
+++ lib/breadcrumbs_on_rails/version.rb
@@ -19,7 +19,7 @@ module BreadcrumbsOnRails
   module Version
     MAJOR = 1
     MINOR = 0
-    PATCH = 1
+    PATCH = 0
     BUILD = nil
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
