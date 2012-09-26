a2d7328de5cdf006be19cf9e6704f83d4d094b5f
Martin Pärtel <martin.partel@gmail.com>
2012-09-26 05:16:22 +0300

    Render element without <a> if path == nil.

--- lib/breadcrumbs_on_rails/breadcrumbs.rb
+++ lib/breadcrumbs_on_rails/breadcrumbs.rb
@@ -87,7 +87,11 @@ module BreadcrumbsOnRails
       end
 
       def render_element(element)
-        content = @context.link_to_unless_current(compute_name(element), compute_path(element), element.options)
+        if element.path == nil
+          content = compute_name(element)
+        else
+          content = @context.link_to_unless_current(compute_name(element), compute_path(element), element.options)
+        end
         if @options[:tag]
           @context.content_tag(@options[:tag], content)
         else
