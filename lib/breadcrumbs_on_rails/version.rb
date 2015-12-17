45fc365c2e9d7da39788eeb48a6cd16314b98914
stopanko <stepander007@gmail.com>
2015-12-17 19:41:57 +0200

    up version

--- lib/breadcrumbs_on_rails/version.rb
+++ lib/breadcrumbs_on_rails/version.rb
@@ -11,7 +11,7 @@ module BreadcrumbsOnRails
   module Version
     MAJOR = 2
     MINOR = 3
-    PATCH = 1
+    PATCH = 2
     BUILD = nil
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
