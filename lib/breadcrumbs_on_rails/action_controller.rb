84c9a28bb28f48de22cf7752dad4d9712acdacbd
Simone Carletti <weppos@weppos.net>
2020-01-31 22:08:43 +0100

    Remove undocumented and undecifrable feature
    The me of 2020 can't even understand what the me of 2009 wanted.
    
    Closes #105

--- lib/breadcrumbs_on_rails/action_controller.rb
+++ lib/breadcrumbs_on_rails/action_controller.rb
@@ -31,46 +31,9 @@ module BreadcrumbsOnRails
       @breadcrumbs ||= []
     end
 
-    module Utils
-
-      def self.instance_proc(string)
-        if string.kind_of?(String)
-          proc { |controller| controller.instance_eval(string) }
-        else
-          string
-        end
-      end
-
-      # This is a horrible method with a horrible name.
-      #
-      #   convert_to_set_of_strings(nil, [:foo, :bar])
-      #   # => nil
-      #   convert_to_set_of_strings(true, [:foo, :bar])
-      #   # => ["foo", "bar"]
-      #   convert_to_set_of_strings(:foo, [:foo, :bar])
-      #   # => ["foo"]
-      #   convert_to_set_of_strings([:foo, :bar, :baz], [:foo, :bar])
-      #   # => ["foo", "bar", "baz"]
-      #
-      def self.convert_to_set_of_strings(value, keys)
-        if value == true
-          keys.map(&:to_s).to_set
-        elsif value
-          Array(value).map(&:to_s).to_set
-        end
-      end
-
-    end
-
     module ClassMethods
 
       def add_breadcrumb(name, path = nil, filter_options = {})
-        # This isn't really nice here
-        if eval = Utils.convert_to_set_of_strings(filter_options.delete(:eval), %w(name path))
-          name = Utils.instance_proc(name) if eval.include?("name")
-          path = Utils.instance_proc(path) if eval.include?("path")
-        end
-
         element_options = filter_options.delete(:options) || {}
 
         before_action(filter_options) do |controller|
